914abfbca0be0ed5c8b1ba12a06e764ebecb2454
drew <bangold@proton.me>
2022-08-28 18:00:43 -0500

    CAMERA WORKS!

--- level.py
+++ level.py
@@ -41,10 +41,10 @@ class CameraGroup(pygame.sprite.Group):
     def custom_draw(self, player):
         self.offset.x = player.rect.centerx - SCREEN_WIDTH / 2
         self.offset.y = player.rect.centery - SCREEN_HEIGHT / 2
-        
+
         for layer in LAYERS.values():
             for sprite in self.sprites():
                 if sprite.z == layer:
                     offset_rect = sprite.rect.copy()
                     offset_rect.center -= self.offset
-                    self.display_surface.blit(sprite.image, sprite.rect)
+                    self.display_surface.blit(sprite.image, offset_rect,) 
