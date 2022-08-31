f1ff67473d3b6a9c6e7a8415c565ad2fb840268c
drew <bangold@proton.me>
2022-08-30 22:48:24 -0500

    added particle effects and inventory logic

--- sprites.py
+++ sprites.py
@@ -39,8 +39,25 @@ class WildFlower(Generic):
         super().__init__( pos, surf, groups)
         self.hitbox = self.rect.copy().inflate(-20,-self.rect.height * 0.9)
 
+class Particle(Generic):
+    def __init__(self, pos, surf, groups, z, duration = 200):
+        super().__init__(pos, surf, groups, z)
+        self.start_time = pygame.time.get_ticks()
+        self.duration = duration
+
+        # white surface
+        mask_surf = pygame.mask.from_surface(self.image)
+        new_surf = mask_surf.to_surface()
+        new_surf.set_colorkey((0,0,0,))
+        self.image = new_surf
+
+    def update(self,dt):
+        current_time = pygame.time.get_ticks()
+        if current_time - self.start_time > self.duration:
+            self.kill()
+
 class Tree(Generic):
-    def __init__(self, pos, surf, groups, name):
+    def __init__(self, pos, surf, groups, name, player_add):
         super().__init__(pos, surf, groups)
 
         #tree attributes
@@ -56,6 +73,8 @@ class Tree(Generic):
         self.apple_sprites = pygame.sprite.Group()
         self.create_fruit()
 
+        self.player_add = player_add
+
     def damage(self):
 
         #damaging the tree
@@ -64,14 +83,23 @@ class Tree(Generic):
         # remove an apple
         if len(self.apple_sprites.sprites()) > 0:
             random_apple = choice(self.apple_sprites.sprites())
+            Particle(
+                pos = random_apple.rect.topleft,
+                surf = random_apple.image,
+                groups = self.groups()[0], 
+                z = LAYERS['fruit'])
+            self.player_add('apple')
             random_apple.kill()
 
     def check_death(self):
         if self.health <= 0:
+            Particle(self.rect.topleft, self.image, self.groups()[0], LAYERS['fruit'], duration = 200)
             self.image = self.stump_surface
             self.rect = self.image.get_rect(midbottom = self.rect.midbottom)
             self.hitbox = self.rect.copy().inflate(-10,-self.rect.height * 0.6)
             self.alive = False
+            self.player_add('wood')
+
 
     def update(self,dt):
         if self.alive:
