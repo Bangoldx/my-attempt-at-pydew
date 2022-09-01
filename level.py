3df42c7e2414844bff2ba7b3b91038cea08bc3ed
drew <bangold@proton.me>
2022-08-31 23:04:10 -0500

    soil sucks

--- level.py
+++ level.py
@@ -1,5 +1,4 @@
 import pygame
-from transition import Transition
 from settings import *
 from player import Player
 from overlay import Overlay
@@ -7,6 +6,7 @@ from sprites import Generic, Water, WildFlower, Tree, Interaction
 from pytmx.util_pygame  import load_pygame
 from support import *   
 from transition import Transition
+from soil import SoilLayer
 
 class Level:
     def __init__(self):
@@ -20,6 +20,7 @@ class Level:
         self.tree_sprites = pygame.sprite.Group()
         self.interaction_sprites = pygame.sprite.Group()
 
+        self.soil_layer = SoilLayer(self.all_sprites)
         self.setup()
         self.overlay = Overlay(self.player)
         self.transition = Transition(self.reset, self.player)
@@ -70,7 +71,8 @@ class Level:
                     group = self.all_sprites, 
                     collision_sprites = self.collision_sprites,
                     tree_sprites = self.tree_sprites,
-                    interaction = self.interaction_sprites)
+                    interaction = self.interaction_sprites,
+                    soil_layer = self.soil_layer)
 
             if obj.name == 'Bed':
                 Interaction((obj.x,obj.y), (obj.width,obj.height), self.interaction_sprites, obj.name)
--- player.py
+++ player.py
@@ -1,11 +1,10 @@
-from telnetlib import STATUS
 import pygame
 from settings import *
 from support import *
 from timer import Timer
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, group, collision_sprites, tree_sprites, interaction):
+    def __init__(self, pos, group, collision_sprites, tree_sprites, interaction, soil_layer):
         super().__init__(group)
         
         self.import_assets()
@@ -56,11 +55,11 @@ class Player(pygame.sprite.Sprite):
         self.tree_sprites = tree_sprites
         self.interaction = interaction
         self.sleep = False
+        self.soil_layer = soil_layer
 
     def use_tool(self):
-        #print('tool use')
         if self.selected_tool == 'hoe':
-            pass
+            self.soil_layer.get_hit(self.target_pos)
 
         if self.selected_tool == 'axe':
             for tree in self.tree_sprites.sprites():
